7d6a229e8d6d5f1d58ecf6721457046286e6c9c4
NSeydulla <63880789+NSeydulla@users.noreply.github.com>
2023-02-06 00:09:07 +0600

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,126 +1,96 @@
-from config import *
-import random
+import pygame
+from config import sc
+from time import time, sleep
+from random import randint as rint
 
 def CRP(pos): # Create Rect Pos
-	return pos*cell_size+border_size*(pos+1)
+	return pos*sc.cell_size+(pos+1)*sc.border_size
 
-class Table(object):
-	def __init__(self):
-		super(Table, self).__init__()
-		self.cells = [[Cell(0, j, i) for j in range(cells_num)] for i in range(cells_num)]
-		self = self.random_put(self)
-		self.score = 0
-		self.already_winned = False
-		self.moves = [[[i, j, CRP(j), CRP(i), CRP(j), CRP(i), self.cells[i][j]] for j in range(cells_num)] for i in range(cells_num)]
-		f = open('record.txt')
-		readed = f.readline()
-		self.record = 0
-		if readed.isdigit():
-			self.record = int(readed)
-		f.close()
+pygame.font.init()
+font = pygame.font.Font(None, int(sc.cell_size*0.7))
 
-	def get(self, x, y):
-		if x in range(cells_num) and y in range(cells_num):
-			return self.cells[y][x]
-		return Out_map
+def text_speech(sc, text, color, pos):
+	rendered_text = font.render(text, True, color)
+	sc.blit(rendered_text, rendered_text.get_rect(center=pos))
 
-	def put(self, x, y, cell):
-		self.cells[y][x] = cell
-	def pop(self, x, y):
-		self.cells[y][x] = Cell(0, x, y)
+class Game():
+	def __init__(self, screen):
+		#animate params
+		self.active = True
+		self.isBusy = False
+		self.lastTime = 0
 
-	def random_put(self, tab):
-		if 0 != random.randint(0,3): # 0.25 Шансов что не завспавнится
-			nulls = []
-			for i in tab.cells:
-				for j in i:
-					if j.num == 0:
-						nulls.append(j)
-			if len(nulls) != 0:
-				i = random.randint(0, len(nulls)-1)
-				self.put(nulls[i].x, nulls[i].y, Cell(random.randint(1,2)*2, nulls[i].x, nulls[i].y))
-		return tab
+		#main params
+		self.sc = screen
+		self.cells = [[Cell(self, 0, x, y) for y in range(sc.cells_num)] for x in range(sc.cells_num)]
+		self.spawn()
 
-	def move(self, orien, tab):
-		a = 1 if '-' in orien else -1
-		for i in range(cells_num)[::a]:
-			for j in range(cells_num)[::a]:
-				cell = self.cells[i][j] if 'y' in orien else self.cells[j][i]
-				tab = cell.move(orien, tab, (cell.x, cell.y))
-		tab = self.random_put(tab)
-		return tab
+	def animate(self):
+		t = time()-self.lastTime
+		if t<sc.animate_time:
+			dt = (t/sc.animate_time)
+			self.x = self.cells[self.animatingCell].x+self.dx * dt
+			self.y = self.cells[self.animatingCell].y+self.dy * dt
+			self.cells[self.animatingCell].blit(self.sc, self.x, self.y)
+		else:
+			self.animatingCell = None
+			self.isBusy = False
+
+	def draw(self):
+		self.sc.fill(sc.BG_COLOR)
+		for row in self.cells:
+			for cell in row:
+				cell.draw(self.sc)
+		pygame.display.update()
+
+	def onKeyDown(self, key):
+		for row in self.cells[::-1 if key == 2 else 1]:
+			for cell in row[::-1 if key == 3 else 1]:
+				if cell.num == 0: continue
+				cell.move(key)
+		self.spawn()
+
+	def spawn(self):
+		for i in range(2):
+			x, y = rint(0,sc.cells_num-1), rint(0,sc.cells_num-1)
+			if self.cells[x][y].num == 0:
+				self.cells[x][y].double()
+				if rint(0,3)==0:self.cells[x][y].double()
 
 class Cell(object):
-	def __init__(self, num, x, y):
-		super(Cell, self).__init__()
+	def __init__(self, Game, num, x, y):
+		self.x, self.y = x, y
+		self.Game = Game
 		self.num = num
-		if num in BG_COLOR:
-			self.BG_COLOR = BG_COLOR[num]
-			self.CELL_COLOR = CELL_COLOR[num]
-		else:
-			self.BG_COLOR = BLACK
-			self.CELL_COLOR = WHITE
-		self.x = x
-		self.y = y
+		self.CELL_COLOR = sc.CELL_COLOR.get(num, (0,0,0))
+		self.TEXT_COLOR = sc.TEXT_COLOR.get(num, (255,255,255))
+		self.rect = pygame.Rect(CRP(x), CRP(y), sc.cell_size, sc.cell_size)
 
-	def plus(self, tab):
-		self.num *= 2
-		tab.score += self.num
-		if tab.score > tab.record:
-			tab.record = tab.score
-			f = open('record.txt', 'w')
-			f.write(str(tab.record))
-			f.close()
-		if self.num in BG_COLOR:
-			self.BG_COLOR = BG_COLOR[self.num]
-			self.CELL_COLOR = CELL_COLOR[self.num]
-		else:
-			self.BG_COLOR = BLACK
-			self.BG_COLOR = WHITE
-		return tab
+	def update(self):
+		self.CELL_COLOR = sc.CELL_COLOR.get(self.num, (0,0,0))
+		self.TEXT_COLOR = sc.TEXT_COLOR.get(self.num, (255,255,255))
 
-	def move(self, orien, tab, old_pos):
-		orien_x, orien_y = 0, 0
-		if 'x' in orien:  orien_x =   1 if orien == '+x' else -1
-		else:             orien_y =   1 if orien == '+y' else -1
-		cell = tab.get(self.x+orien_x, self.y+orien_y)
-		if cell.num == 0:
-			tab.pop(self.x, self.y)
-			self.x += orien_x
-			self.y += orien_y
-			tab.put(self.x, self.y, self)
-			tab = self.move(orien, tab, old_pos)
-			tab.moves[old_pos[1]][old_pos[0]] = [
-				self.x,
-				self.y,
-				CRP(self.x),
-				CRP(self.y),
-				CRP(old_pos[0]),
-				CRP(old_pos[1]),
-				self] #, tab.moves[old_pos[1]][old_pos[0]][2]
+	def set(self, num=0):
+		self.num = num
+		self.update()
 
-		elif cell.num == self.num:
-			tab.pop(self.x, self.y)
-			tab = tab.get(cell.x, cell.y).plus(tab)
-			tab.moves[old_pos[1]][old_pos[0]] = [
-				cell.x,
-				cell.y,
-				CRP(cell.x),
-				CRP(cell.y),
-				CRP(old_pos[0]),
-				CRP(old_pos[1]),
-				cell]
+	def double(self, num=2):
+		if self.num == 0: self.num=num
+		else: self.num *= 2
+		self.update()
 
-		return tab
+	def move(self, key):
+		xl = [-1,0,1,0]
+		yl = [0,-1,0,1]
+		x = self.x+xl[key]
+		y = self.y+yl[key]
+		if (not 0<=x<sc.cells_num) or (not 0<=y<sc.cells_num): return
+		if self.Game.cells[x][y].num in [self.num, 0]:
+			self.Game.cells[x][y].double(self.num)
+			self.set()
+			self.Game.cells[x][y].move(key)
 
-	def isCanMove(self, tab):
-		a = [0, self.num]
-		up    = tab.get(self.x, self.y-1).num in a
-		down  = tab.get(self.x, self.y+1).num in a
-		left  = tab.get(self.x-1, self.y).num in a
-		right = tab.get(self.x+1, self.y).num in a
-
-		if up or down or left or right: return True
-		return False
-
-Out_map = Cell(3, 'wr', 'wr')
+	def draw(self, screen):
+		pygame.draw.rect(screen, self.CELL_COLOR, self.rect)
+		if self.num != 0:
+			text_speech(screen, str(self.num), self.TEXT_COLOR, self.rect.center)
--- main.py
+++ main.py
@@ -1,112 +1,24 @@
 import pygame
-import time
 from config import *
-from classes import *
+from classes import Game
 
-tab = Table()
-
-def text_speech(font, text, color, xy, bold=0):
-	font.set_bold(bold)
-	rendered_text = font.render(text, True, color)
-	text_rect = rendered_text.get_rect(center=xy)
-	sc.blit(rendered_text, text_rect)
-
-def game_loop():
-	global gamePlay, tab, win
-	pressed='+x'
+def main():
 	while 1:
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_F4 and event.mod == 256):
-				pygame.quit(); quit()
-
-			if event.type == pygame.KEYDOWN and gamePlay and event.key in KEYS and pressed == '':
-				tab = tab.move(KEYS[event.key], tab)
-				pressed = KEYS[event.key]
-
-			if (event.type == pygame.MOUSEBUTTONDOWN and
-				event.button == 1 and
-				sc_rect.collidepoint(event.pos) and
-				not gamePlay):
-
-				if not can_moves:
-					tab = Table()
-				gamePlay = True
-				win = False
-
-
-		can_moves = False
-		for i in range(cells_num):
-			for j in range(cells_num):
-				cell = tab.get(i, j)
-				if cell.num != 0:
-					if cell.isCanMove(tab):
-						can_moves = True
-					if cell.num == 2048 and not tab.already_winned:
-						win = True
-						gamePlay = False
-						tab.already_winned = True
-
-		# Рисовальня
-		if pressed!='':
-			sc.fill(BG_COLOR[0])
-			for i in range(cells_num+1):
-				coord = (cell_size+border_size)*i
-				pygame.draw.rect(sc, BG_GAME, (coord, 0, border_size, sc_w))
-				pygame.draw.rect(sc, BG_GAME, (0, coord, sc_w, border_size))
-				if i == cells_num:
-					pygame.draw.rect(sc, BG_GAME, (0, coord, sc_w, sc_h-coord))
-
-			text_speech(font_25, f'Score: {tab.score}', (99, 90, 81), (sc_w//4, sc_h-16))
-			text_speech(font_25, f'record: {tab.record}', (99, 90, 81), (sc_w//2+sc_w//4, sc_h-16))
-			if not can_moves:
-				gamePlay = False
-				text_speech(font_25, 'Игра окончена!', (250, 85, 85), (sc_w//2, sc_h//2-13))
-			if win:
-				text_speech(font_25, 'Вы выйграли!',       (50,120,50), (sc_w//2, sc_h//2-27))
-				text_speech(font_25, 'Можете нажать и продолжить!', (50,120,50), (sc_w//2, sc_h//2-13))
-
-
-			a = -1 if '+' in pressed else 1
-			for b in range(int((cell_size+border_size)*4/speed)):
-				for i in range(cells_num)[::a]:
-					for j in range(cells_num)[::a]:
-						for g in range(cells_num+1):
-							coord = (cell_size+border_size)*g
-							pygame.draw.rect(sc, BG_GAME, (coord, 0, border_size, sc_w))
-							pygame.draw.rect(sc, BG_GAME, (0, coord, sc_w, border_size))
-							if g == cells_num:
-								pygame.draw.rect(sc, BG_GAME, (0, coord, sc_w, sc_h-coord))
-
-						x, y = (j, i) if 'y' in pressed else (i, j)
-						moving_cell = tab.moves[y][x]
-						
-						pygame.draw.rect(sc, BG_COLOR[0], pygame.Rect(moving_cell[4], moving_cell[5], cell_size, cell_size))
-						
-						x_or_y = 1 if 'y' in pressed else 0
-						if moving_cell[4+x_or_y] != moving_cell[2+x_or_y]:
-							moving_cell[4+x_or_y] += speed if '+' in pressed else -speed
-
-						cell_rect = pygame.Rect(moving_cell[4], moving_cell[5], cell_size, cell_size)
-						cell_rect = pygame.draw.rect(sc, moving_cell[6].BG_COLOR, cell_rect)
-						text_speech(font, str(moving_cell[6].num), moving_cell[6].CELL_COLOR, (cell_rect.width//2+cell_rect.x, cell_rect.height//2+cell_rect.y))
-
-						pygame.display.update()
-						pygame.time.delay(1)
-			tab.moves = [[[i, j, CRP(j), CRP(i), CRP(j), CRP(i), tab.cells[i][j]] for j in range(cells_num)] for i in range(cells_num)]
-
-			pressed = ''
-			pygame.display.update()
+			if event.type == pygame.QUIT: return
+			if event.type == pygame.KEYDOWN and event.key in KEYS and not Game.isBusy and Game.active:
+				Game.onKeyDown(KEYS.index(event.key))
+		Game.draw()
 		clock.tick(30)
 
 pygame.init()
-pygame.font.init()
-sc = pygame.display.set_mode((sc_w, sc_h))
+screen = pygame.display.set_mode((sc.w, sc.h))
 pygame.display.set_caption("2048 game")
+
+Game = Game(screen)
 clock = pygame.time.Clock()
 
-font =    pygame.font.Font(None, 20)
-font_25 = pygame.font.Font(None, 27)
+KEYS = [pygame.K_LEFT, pygame.K_UP, pygame.K_RIGHT, pygame.K_DOWN]
 
-game_loop()
+main()
 pygame.quit()
-quit()
